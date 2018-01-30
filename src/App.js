4d8f525357a105c4dc5acc0efc92eb063f947584
Cococody <mayo.cody@gmail.com>
2018-01-30 17:05:53 -0600

    finished the project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 
-import Header from './components/subcomponents/Header'
-import Footer from './components/subcomponents/Footer'
-import Home from './components/Home'
-import Search from './components/Search'
-import Blog from './components/Blog'
-import Add from './components/Add'
-import Edit from './components/Edit'
-import User from './components/User'
-import NewUser from './components/NewUser'
+import Header from "./components/subcomponents/Header";
+import Footer from "./components/subcomponents/Footer";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import Blog from "./components/Blog";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
+import User from "./components/User";
+import NewUser from "./components/NewUser";
+import BlogThumb from "./components/subcomponents/BlogThumb";
 
 class App extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header />
         <Switch>
-          <Route path='/user/:id/edit' component={NewUser} />
-          <Route path='/user/:id' component={User} />
-          <Route path='/user' component={NewUser} />
-          <Route path='/blog/:id' component={Blog} />
-          <Route path='/add' component={Add} />
-          <Route path='/edit/:id' component={Edit} />
-          <Route path='/search' component={Search} />
-          <Route path='/' component={Home} />
+          <Route path="/user/:id/edit" component={NewUser} />
+          <Route path="/user/:id" component={User} />
+          <Route path="/user" component={NewUser} />
+          <Route path="/blog/:id" component={Blog} />
+          <Route path="/add" component={Add} />
+          <Route path="/edit/:id" component={Edit} />
+          <Route path="/search" component={Search} />
+          <Route path="/" component={Home} />
         </Switch>
         <Footer />
       </div>
